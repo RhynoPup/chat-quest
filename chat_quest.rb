67c0e7a6353cb1fae09cf2a6bb4fbb6ef680a8b1
Ryan Mohrman <ryan.mohrman@gmail.com>
2017-12-07 01:56:03 -0700

    updated game to have a cleaner move function and check for room requirements. Initial look and take commands added

--- chat_quest.rb
+++ chat_quest.rb
@@ -1,129 +1,69 @@
 require_relative 'classes'
 
-#class Room
-#  attr :description, true
-#  attr :inventory, true
-#  attr :directions, true
-#  attr :requirements, true
-#
-#  def update(des,inv,dir,req)
-#    self.description = des
-#    self.inventory = inv
-#    self.directions = dir
-#    self.requirements = req
-#  end
-#end
+game = App.new
 
 room1 = Room.new
 room2 = Room.new
 room3 = Room.new
 
-room1.update("This is room1. Welcome!",[""],{"nw" => room3, "w" => room2},[""])
-room2.update("This is room2.",["spoon"],{"e" => room1},[""])
-room3.update("This is room3.",[""],{"se" => room1},[""])
-
-class App 
-  def self.hello
-    puts "hello to you too"
-  end
-  def self.invalidCommand
-    puts "Invalid command"
-  end
-  def self.moveCommand(direction)
-    puts "You said go, " + direction
-  end
-end
-
-#class Player
-#  attr :position, true
-#
-#  def initialize(room)
-#    self.position = room
-#  end
-#end
+room1.update("This is room1. Welcome!",[],{"nw" => room3, "w" => room2},[])
+room2.update("This is room2.",["spoon"],{"e" => room1},["fork"])
+room3.update("This is room3.",["fork"],{"se" => room1},[])
 
 player1 = Player.new(room1)
 
 loop do
+  print "> "
   input = gets.chomp
   command, *params = input.split /\s/
-  case command
+  txt = params.join(" ")
+  case command.downcase
     when 'hello'
       puts App.hello
-      puts player1.position.description
     
     when 'quit'
       break;
     
     when 'go', 'move', 'walk'
-      case params[0].downcase
+      case txt.downcase
         when 'north', 'n'
-          if (player1.position.directions.key?("n"))
-            player1.position = player1.position.directions["n"]
-            puts player1.position.description
-          else
-            puts "You cannot move that way"
-          end
-          #puts App.moveCommand(params[0])
+          game.move("n", player1)
         when 'northwest', 'nw'
-          if (player1.position.directions.key?("nw"))
-            player1.position = player1.position.directions["nw"]
-            puts player1.position.description
-          else
-            puts "You cannot move that way"
-          end
-          #puts App.moveCommand(params[0])
+          game.move("nw", player1)
         when 'west', 'w'
-          if (player1.position.directions.key?("w"))
-            player1.position = player1.position.directions["w"]
-            puts player1.position.description
-          else
-            puts "You cannot move that way"
-          end
-          #puts App.moveCommand(params[0])
+          game.move("w", player1)
         when 'southwest', 'sw'
-          if (player1.position.directions.key?("sw"))
-            player1.position = player1.position.directions["sw"]
-            puts player1.position.description
-          else
-            puts "You cannot move that way"
-          end
-          #puts App.moveCommand(params[0])
+          game.move("sw", player1)
         when 'south', 's'
-          if (player1.position.directions.key?("s"))
-            player1.position = player1.position.directions["s"]
-            puts player1.position.description
-          else
-            puts "You cannot move that way"
-          end
-          #puts App.moveCommand(params[0])
+          game.move("s", player1)
         when 'southeast', 'se'
-          if (player1.position.directions.key?("se"))
-            player1.position = player1.position.directions["se"]
-            puts player1.position.description
-          else
-            puts "You cannot move that way"
-          end
-          #puts App.moveCommand(params[0])
+          game.move("se", player1)
         when 'east', 'e'
-          if (player1.position.directions.key?("e"))
-            player1.position = player1.position.directions["e"]
-            puts player1.position.description
-          else
-            puts "You cannot move that way"
-          end
-          #puts App.moveCommand(params[0])
+          game.move("e", player1)
         when 'northeast', 'ne'
-          if (player1.position.directions.key?("ne"))
-            player1.position = player1.position.directions["ne"]
-            puts player1.position.description
-          else
-            puts "You cannot move that way"
-          end
-          #puts App.moveCommand(params[0])
+          game.move("ne", player1)
         else
           puts App.invalidCommand
-        end
+      end
+
+    when 'look'
+      if (player1.position.inventory.empty?)
+        puts puts player1.position.description
+      else
+        puts player1.position.description
+        puts puts "This room has a " + player1.position.inventory[0] + " in it."
+      end
+
+    when 'inventory', 'items'
+      puts player1.inventory
+
+    when 'take', 'grab'
+      if(!txt.empty? && player1.position.inventory.include?(txt.downcase))
+        player1.addItem(txt.downcase)
+        player1.position.inventory.delete(txt.downcase)
+      else
+        puts puts "I do not understand"
+      end
     
     else
       puts App.invalidCommand
--- classes.rb
+++ classes.rb
@@ -1,8 +1,44 @@
+class App 
+  def self.hello
+    puts "hello to you too"
+  end
+  def self.invalidCommand
+    puts "Invalid command"
+  end
+  def self.moveCommand(direction)
+    puts "You said go, " + direction
+  end
+
+  def move(direction, player)
+    if (player.position.directions.key?(direction))
+      req = player.position.directions[direction].requirements
+      if (req == (req & player.inventory) || req == (player.inventory & req))
+        player.position = player.position.directions[direction]
+        puts puts player.position.description
+      else
+        puts puts "Your path is blocked"
+      end
+    else
+      puts puts "You cannot move that way"
+    end
+  end
+end
+
 class Player
   attr :position, true
+  attr :inventory, true
 
   def initialize(room)
     self.position = room
+    self.inventory = Array.new
+  end
+
+  def addItem(item)
+    self.inventory.push(item)
+  end
+
+  def removeItem(item)
+    self.inventory.delete(item)
   end
 end
 
